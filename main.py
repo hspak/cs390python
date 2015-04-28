dba4f875cb11b8966e7685ae2604be71e7146656
Hong Shick Pak <hong@hspak.com>
2015-04-28 17:29:31 -0400

    sessions and email verification work

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, escape, redirect, url_for
+from parse_rest.user import User
+from parse_setup import setup
+
 app = Flask(__name__)
+app.secret_key = 'Super secret string'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        user = escape(session['username'])
+        return render_template('index.html', user=user)
+    else:
+        return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    u = User.signup(username, password, email=email)
+    u.save()
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return "post"
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        session['password'] = password
+        u = User.login(username, password)
+        u.save()
+        return redirect(url_for('index'))
     else:
-        return render_template('login.html')
+        if 'username' in session:
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', new=True)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.debug = True
--- parse_setup.py
+++ parse_setup.py
@@ -1,13 +1,16 @@
+from flask import Blueprint
 from parse_rest.datatypes import Object
 from parse_rest.connection import register
 
-class FaceFa(Object):
-    pass
+setup = Blueprint('setup', __name__)
+
+# class FaceFa(Object):
+    # pass
 
 APPLICATION_ID = "h2Co5EGV2YoBuPL2Cl7axkcLE0s9FNKpaPcpSbNm"
 REST_API_KEY = "o59euguskg7BBNZlFEuVxTNL0u93glStq7memfVH"
 
 register(APPLICATION_ID, REST_API_KEY)
 
-gameScore = FaceFa(what=2, ass=1337, poop='shit', yolo=False)
-gameScore.save()
+# gameScore = FaceFa(what=2, ass=1337, poop='shit', yolo=False)
+# gameScore.save()
